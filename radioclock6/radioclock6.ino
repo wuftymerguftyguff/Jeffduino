9023fe24c39d994112f189767035bca569af0951
Jeff Arthur <wuftymerguftyguff@gmail.com>
2014-09-24 20:04:53 +0100

    radio 6 - just pulse length for high and low
    two interrups, this is the way to go i think.

--- radioclock6/radioclock6.ino
+++ radioclock6/radioclock6.ino
@@ -70,20 +70,23 @@ void risingPulse() {
   secondBitOffset = ((currentMillis - previousMillis) )/ 100;
   second[secondBitOffset] = false;
   //print the length of the last low pulse
-  //Serial.println("RISING");
-  //Serial.print("LOW: ");
+  //Serial.print("RISING");
+  Serial.print("LOW: ");
   Serial.print(pulseWidth);
   Serial.print(" ");
 }
 
 void fallingPulse() {
+  pulseChange(); 
+  Serial.print("HIGH: ");
+  Serial.print(pulseWidth);
   if ( lastPulseWidth >= 450 && pulseWidth >= 450 ) {
     TOM = true;
   }
-  if ( lastPulseWidth >= 450 ) {
+  if ( pulseWidth >= 450 ) {
     TOS = true;
   }
-  pulseChange(); 
+
     //print the length of the last high pulse
   //Serial.println(secondBitOffset);
   //Serial.print("HIGH: ");
@@ -102,7 +105,7 @@ void setup() {
 // this looks reversed asthe module reversed the serial output of the carrier state
 attachInterrupt(0, risingPulse, FALLING) ;
 attachInterrupt(1, fallingPulse, RISING) ;
-Serial.begin(57600);           // set up Serial library at 9600 bps
+Serial.begin(19200);           // set up Serial library at 9600 bps
 Serial.println("Clock Starting");  // prints hello with ending line break 
 pinMode(ledPin, OUTPUT); 
 // set all the values of the current second to 1
@@ -112,43 +115,19 @@ memset(&second[0], 0x01, sizeof(second));
 
 void loop() {
   
-  // here is where you'd put code that needs to be running all the time.
-
-  // check to see if it's time to blink the LED; that is, if the 
-  // difference between the current time and last time you blinked 
-  // the LED is bigger than the interval at which you want to 
-  // blink the LED.
-  
-  currentMillis = millis();
- 
-  if(currentMillis - previousMillis > interval || TOM || TOS) {
-    // save the last time you blinked the LED 
-    previousMillis = currentMillis;   
-    //Serial.println("Reset second Template ");
-    Serial.println(" ");
-    for (int i=0;i<10;i++) {
-      Serial.print(i);
-      Serial.print(":");
-      Serial.print(second[i]);
-      Serial.print("  ");
-    }
- 
-    memset(&second[0], 0x01, sizeof(second));
-    previousMillis = currentMillis; 
-    //do this every interval
-  }
-  
-  if ( TOS == true ) {  
-    Serial.print("TOS ");  
-    TOS = false;
-    previousMillis = millis();
-  }
-  
-  if ( TOM == true ) {  
-    Serial.print("TOM ");  
+   if ( TOM == true ) {  
+    Serial.print("\nTOM *************\n");  
     TOM = false;
     previousMillis = millis();
   }
+  
+  if ( TOS == true ) {  
+    Serial.print("\nTOS ");  
+    TOS = false;
+    memset(&second[0], 0x01, sizeof(second));
+  }
+  
+ 
  
 }
 
